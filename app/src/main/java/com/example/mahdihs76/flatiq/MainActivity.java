b671551672b4eca731ab831dfb6cd3bfee16bfd4
mahdihs76 <m.hasan1395@student.sharif.edu>
2017-09-13 22:51:12 +0430

    Calligraphy Library Added!

--- app/src/main/java/com/example/mahdihs76/flatiq/MainActivity.java
+++ app/src/main/java/com/example/mahdihs76/flatiq/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mahdihs76.flatiq;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Calligraphy
+
+        // 1- Add it to onCreate() activity :
+
+        //CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+        //        .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+        //        .setFontAttrId(R.attr.fontPath)
+        //        .build()
+        //);
+
+        // 2- Add it to activity class :
+
+        //@Override
+        //protected void attachBaseContext(Context newBase) {
+        //    super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+        //}
+
+        // 3- usage (Add font to assets/fonts:
+        // <TextView fontPath="fonts/MyFont.ttf"/>
+
     }
+
+
+
 }
