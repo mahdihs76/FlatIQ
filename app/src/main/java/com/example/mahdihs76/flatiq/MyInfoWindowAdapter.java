0ec878f73315ff2dd2e9a1ecc770eb7c80a24484
kossarNJ <kossarna@gmail.com>
2017-09-14 20:52:10 +0430

    map custom info window added.

---
+++ app/src/main/java/com/example/mahdihs76/flatiq/MyInfoWindowAdapter.java
@@ -0,0 +1,37 @@
+package com.example.mahdihs76.flatiq;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by kosar on 9/14/17.
+ */
+
+public class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+    Activity activity;
+    Button button;
+
+    public MyInfoWindowAdapter(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        View v = activity.getLayoutInflater().inflate(R.layout.map_test, null);
+        button = (Button) v.findViewById(R.id.map_button);
+        Log.i("debug", "getInfoWindow: " + button);
+        button.setText(marker.getTitle());
+        return v;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
