7b30665bb7702e3f4f0621f3f51d43f6478e217c
hamedshirvani <30793780+hamedshirvani@users.noreply.github.com>
2017-09-15 22:27:27 +0430

    user pass

--- app/src/main/java/com/example/mahdihs76/flatiq/view/page/login/LoginActivity.java
+++ app/src/main/java/com/example/mahdihs76/flatiq/view/page/login/LoginActivity.java
@@ -12,9 +12,12 @@ import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.mahdihs76.flatiq.R;
+import com.example.mahdihs76.flatiq.constant.BacktoryAdminUser;
+import com.example.mahdihs76.flatiq.constant.Constant;
 import com.example.mahdihs76.flatiq.view.page.main.MainActivity;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
@@ -25,6 +28,8 @@ public class LoginActivity extends FragmentActivity{
     private static int pagePosition = 0;
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private TextView userName;
+    private TextView password;
     private TextView registerButton;
 
     @Override
@@ -39,12 +44,19 @@ public class LoginActivity extends FragmentActivity{
                 .build());
 
         registerButton = (TextView) findViewById(R.id.register_button);
+        userName = (TextView) findViewById(R.id.username);
+        password = (TextView) findViewById(R.id.password);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                if (userName.getText().toString().equals(BacktoryAdminUser.USERNAME) && password.getText().toString().equals(BacktoryAdminUser.PASSWORD))
+                {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "نام کاربری و رمز عبور مطابقت ندارند", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
